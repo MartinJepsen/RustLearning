d6a825e0e14839c55df16b7a1afd73db1ee43c4f
MJ <mjeps17@student.aau.dk>
2023-03-27 20:49:03 +0200

    add random number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io; // imports io into the namespace
+use rand::Rng;
 
 fn main() {
     // main() is the entry point to the programme
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100)
+
     println!("Please input your guess.");
     
     // let mut creates a mutable variable
@@ -13,6 +17,9 @@ fn main() {
 
     // read_line(&mut guess) means that we store the user input in the guess variable
     // The & indicates a reference. This lets multiple parts of the programme access the same data without having to copy it.
+    // The .expect function tells the programme what to do if we get an exception.
+    // This is because read_line returns Result which is an enum. This enum has either the user input or an error
+    // and .expect handles this error.
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
